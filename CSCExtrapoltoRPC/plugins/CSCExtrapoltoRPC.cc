7e5bcdde4b0608206385ba256acbdf32a21fa58e
Jieun <crown_1225@naver.com>
2019-04-10 11:24:08 +0200

    RPC loop in CSC loop

--- CSCExtrapoltoRPC/plugins/CSCExtrapoltoRPC.cc
+++ CSCExtrapoltoRPC/plugins/CSCExtrapoltoRPC.cc
@@ -20,6 +20,7 @@
 // system include files
 #include <memory>
 #include <iostream>
+#include <math.h>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -93,11 +94,18 @@ class CSCExtrapoltoRPC : public edm::one::EDAnalyzer<edm::one::SharedResources> 
     TH1D *RE31Nrechit;
     TH1D *RE41Nrechit;
 
+    TH1D *h_S3NDigis;
+    TH1D *h_S4NDigis;
+    TH1D *h_S3Nrechits;
+    TH1D *h_S4Nrechits;
+
     unsigned int b_EVENT, b_RUN, b_LUMI;
     unsigned int b_numberofDigis;
 
     unsigned int b_fNDigis;
     unsigned int b_bNDigis;
+    unsigned int b_ME11NDigis;
+    unsigned int b_ME21NDigis;
     unsigned int b_ME31NDigis;
     unsigned int b_ME41NDigis;
 
@@ -148,12 +156,10 @@ CSCExtrapoltoRPC::getCSCGlobalPosition(CSCDetId rawId, const CSCCorrelatedLCTDig
 }
 
 GlobalPoint
-//CSCExtrapoltoRPC::getRPCGlobalPosition(RPCDetId rpcId, const RPCRecHit& rpcIt) const{
-CSCExtrapoltoRPC::getRPCGlobalPosition(RPCDetId rpcId, const RPCRecHit& recHit) const{
+CSCExtrapoltoRPC::getRPCGlobalPosition(RPCDetId rpcId, const RPCRecHit& rpcIt) const{
 
   RPCDetId rpcid = RPCDetId(rpcId);
-//  const LocalPoint& rpc_lp = rpcIt.localPosition();
-  const LocalPoint& rpc_lp = recHit.localPosition();
+  const LocalPoint& rpc_lp = rpcIt.localPosition();
   const GlobalPoint& rpc_gp = rpcGeo->idToDet(rpcid)->surface().toGlobal(rpc_lp);
  
   return rpc_gp;
@@ -180,8 +186,8 @@ CSCExtrapoltoRPC::CSCExtrapoltoRPC(const edm::ParameterSet& iConfig)
   Ndigis->GetXaxis()->SetTitle("Number of digis per chamber");
   Ndigis->GetYaxis()->SetTitle("Number of digis");
 
-  fNdigis = fs->make<TH1D>("fNdigis", "number of digis per chamber (forward)", 10, 0, 10);
-  bNdigis = fs->make<TH1D>("bNdigis", "number of digis per chamber (backward)", 10, 0, 10);
+//  fNdigis = fs->make<TH1D>("fNdigis", "number of digis per chamber (forward)", 10, 0, 10);
+//  bNdigis = fs->make<TH1D>("bNdigis", "number of digis per chamber (backward)", 10, 0, 10);
 
   ME31NDigis = fs->make<TH1D>("ME31NDigis", "number of digis per chamber (ME3/1)", 10, 0, 10);
   ME31NDigis->GetXaxis()->SetTitle("Number of digis per chamber");
@@ -202,6 +208,22 @@ CSCExtrapoltoRPC::CSCExtrapoltoRPC(const edm::ParameterSet& iConfig)
   RE41Nrechit = fs->make<TH1D>("RE41Nrechit", "number of rechits per chamber (RE4/1)", 10, 0, 10);
   RE41Nrechit->GetXaxis()->SetTitle("Number of rechit per chamber");
   RE41Nrechit->GetYaxis()->SetTitle("Number of rechits");
+
+  h_S3NDigis = fs->make<TH1D>("h_S3NDigis", "number of digis in station 3", 10, 0, 10);
+  h_S3NDigis->GetXaxis()->SetTitle("Number of digis in station 3");
+  h_S3NDigis->GetYaxis()->SetTitle("Number of digis");
+  
+  h_S4NDigis = fs->make<TH1D>("h_S4NDigis", "number of digis in station 4", 10, 0, 10);
+  h_S4NDigis->GetXaxis()->SetTitle("Number of digis in station 4");
+  h_S4NDigis->GetYaxis()->SetTitle("Number of digis");
+
+  h_S3Nrechits = fs->make<TH1D>("h_S3Nrechits", "number of rechits in station 3", 10, 0, 10);
+  h_S3Nrechits->GetXaxis()->SetTitle("Number of rechits in station 3");
+  h_S3Nrechits->GetYaxis()->SetTitle("Number of rechits");
+  
+  h_S4Nrechits = fs->make<TH1D>("h_S4Nrechits", "number of rechits in station 4", 10, 0, 10);
+  h_S4Nrechits->GetXaxis()->SetTitle("Number of rechits in station 4");
+  h_S4Nrechits->GetYaxis()->SetTitle("Number of rechits");
 }
 
 CSCExtrapoltoRPC::~CSCExtrapoltoRPC()
@@ -249,31 +271,140 @@ CSCExtrapoltoRPC::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   std::cout << "\nNew event" << endl;
     for(CSCCorrelatedLCTDigiCollection::DigiRangeIterator csc=corrlcts.product()->begin(); csc!=corrlcts.product()->end(); csc++){  
       CSCCorrelatedLCTDigiCollection::Range range1 = corrlcts.product()->get((*csc).first);
-//       cout << "\nwe are in ranage 1 " << endl;
-      b_numberofDigis = b_fNDigis = b_bNDigis = b_ME31NDigis = b_ME41NDigis = 0;
+      b_numberofDigis = b_fNDigis = b_bNDigis = b_ME11NDigis  = b_ME21NDigis = b_ME31NDigis = b_ME41NDigis = 0;
+      
+      int nRPC = 0;
+      int S3NDigis = 0;
+      int S4NDigis = 0;
+      int S3Nrechits = 0;
+      int S4Nrechits = 0;
+
+      GlobalPoint gp_cscint;
 
       for(CSCCorrelatedLCTDigiCollection::const_iterator lct=range1.first; lct!=range1.second; lct++){
-
         const CSCDetId csc_id((*csc).first.rawId());
 
-        GlobalPoint gp_cscint(getCSCGlobalPosition(csc_id, *lct));  
-        if (csc_id.station() == 3 && csc_id.ring() == 1){
+        gp_cscint = GlobalPoint(0.0,0.0,0.0);
+        gp_cscint = getCSCGlobalPosition(csc_id, *lct); 
+        cout << "CSCGlobalPoint " << gp_cscint << endl;
+
+        b_numberofDigis++;
+
+        if (csc_id.station() == 3) S3NDigis++;
+        if (csc_id.station() == 4) S4NDigis++;
+
+        if (csc_id.station() == 1){
+          if (csc_id.ring() == 4 || csc_id.ring() == 1)
+          b_ME11NDigis++;
+          GlobalPoint gp_ME11(getCSCGlobalPosition(csc_id, *lct));
+          cout << "CSCGlobalposition in ME1/1" << gp_ME11 << "BX: " << lct->getBX() << endl;
+        }
+        else if (csc_id.station() == 2 && csc_id.ring() == 1){
+          b_ME21NDigis++;
+          GlobalPoint gp_ME21(getCSCGlobalPosition(csc_id, *lct));
+          cout << "CSCGlobalposition in ME4/1" << gp_ME21 << "BX: " << lct->getBX() << endl;
+        }
+        else if (csc_id.station() == 3 && csc_id.ring() == 1){
           b_ME31NDigis++;
-          cout << "CSCGlobalposition in ME3/1" << gp_cscint << "BX: " << lct->getBX() << endl;
+          GlobalPoint gp_ME31(getCSCGlobalPosition(csc_id, *lct));
+          cout << "CSCGlobalposition in ME3/1" << gp_ME31 << "BX: " << lct->getBX() << endl;
         }
         else if (csc_id.station() == 4 && csc_id.ring() == 1){
           b_ME41NDigis++;
-          cout << "CSCGlobalposition in ME4/1" << gp_cscint << "BX: " << lct->getBX() << endl;
+          GlobalPoint gp_ME41(getCSCGlobalPosition(csc_id, *lct));
+          cout << "CSCGlobalposition in ME4/1" << gp_ME41 << "BX: " << lct->getBX() << endl;
         }
-//        cout << "Globalfunction intersect: " << gp_cscint << endl;
-      }
+
+        nRPC = 0;
+        S3Nrechits = S4Nrechits = 0;
+        for (RPCRecHitCollection::const_iterator rpcIt = rpcRecHits->begin(); rpcIt != rpcRecHits->end(); rpcIt++) {
+          nRPC++;
+          RPCDetId rpcid = (RPCDetId)(*rpcIt).rpcId();
+
+          if(rpcid.station() == 3) S3Nrechits++;
+          if(rpcid.station() == 4) S4Nrechits++;
+
+          GlobalPoint gp_rpc(0.0,0.0,0.0);
+          gp_rpc = getRPCGlobalPosition(rpcid, *rpcIt);
+
+
+          //if (gp_rpc.x() != 0 or gp_rpc.y() != 0 or gp_rpc.z() != 0) cout << "RPC rechits are here" << gp_rpc << endl;
       
-      Ndigis->Fill(b_numberofDigis);
+          if (rpcid.region() == 0) continue;
+
+          if (rpcid.station() == 3 && rpcid.ring() == 1) cout << "RPCGlobalposition in ME3/1" << gp_rpc << "BX/clSize: " << (*rpcIt).BunchX()  << "/" << (*rpcIt).clusterSize() << endl;
+          if (rpcid.station() == 4 && rpcid.ring() == 1) cout << "RPCGlobalposition in ME4/1" << gp_rpc << "BX/clSize: " << (*rpcIt).BunchX() << "/" << (*rpcIt).clusterSize() << endl;
+          
+
+          if (gp_rpc.x() == 0 && gp_rpc.y() == 0 && gp_rpc.z() == 0 ) continue;
+          if (gp_cscint.x() == 0 && gp_cscint.y() == 0 && gp_cscint.z() == 0 ) continue;
+          cout << "RPCGlobalPoint " << gp_rpc << endl;
+
+
+          float Rx = gp_rpc.x();
+          float Ry = gp_rpc.y();
+        //  float Rz = gp_rpc.z();
+
+          float Cx = gp_cscint.x();
+          float Cy = gp_cscint.y();
+          //float Cz = gp_cscint.z();
+
+          float extrapol2D = sqrt((Rx-Cx)*(Rx-Cx)+(Ry-Cy)*(Ry-Cy));
+
+          if (csc_id.endcap() == 1 && csc_id.station() == 3 && csc_id.ring() == 1 && rpcid.region() == 1 && rpcid.station() == 3 && rpcid.ring() == 1) cout << "distance in forward between ME31 RE31: " << extrapol2D << endl;
+          if (csc_id.endcap() == 1 && csc_id.station() == 4 && csc_id.ring() == 1 && rpcid.region() == 1 && rpcid.station() == 4 && rpcid.ring() == 1) cout << "distance in forward between ME41 RE41: " << extrapol2D << endl;
+             
+          if (csc_id.endcap() == 2 && csc_id.station() == 3 && csc_id.ring() == 1 && rpcid.region() == -1 && rpcid.station() == 3 && rpcid.ring() == 1) cout << "distance in backward between ME31 RE31: " << extrapol2D << endl;
+          if (csc_id.endcap() == 2 && csc_id.station() == 4 && csc_id.ring() == 1 && rpcid.region() == -1 && rpcid.station() == 4 && rpcid.ring() == 1) cout << "distance in backward between ME41 RE41: " << extrapol2D << endl;
+/*
+          int kRoll  = rpcid.roll();
+          int kSubsector  = rpcid.subsector();
+          int kRegion  = rpcid.region();
+          int kStation = rpcid.station();
+          int kRing = rpcid.ring();
+          int kSector = rpcid.sector();
+          int kLayer = rpcid.layer();
+          int bx = (*rpcIt).BunchX();
+          int clSize = (*rpcIt).clusterSize();
+      
+          cout << "I'm here in RPC Region: " << kRegion <<
+                                " Station: " << kStation <<
+                                " Ring: "    << kRing <<
+                                " Sector: "  << kSector <<
+                              " Subsector: " << kSubsector <<
+                                   " Roll: " << kRoll <<
+                                  " Layer: " << kLayer <<
+                             "\tbx,clSize: " << bx << ", " << clSize << endl;    
+          cout << "number of RPCRecHits: " << nRPC << endl;
+      
+          int nrechitRE31 = 0;
+          int nrechitRE41 = 0;
+          if (kRegion != 0){
+            if (kStation == 3 && kRing == 1) nrechitRE31++;    
+            else if(kStation == 4 && kRing == 1) nrechitRE41++;
+      
+          }
+      
+          cout << "RE31: " << nrechitRE31 << " RE41: " << nrechitRE41 << endl;
+          if (nrechitRE31 != 0) RE31Nrechit->Fill(nrechitRE31);
+          if (nrechitRE41 != 0) RE41Nrechit->Fill(nrechitRE41);
+      */
+      
+        }
+ 
+      }
+
       ME31NDigis->Fill(b_ME31NDigis);
       ME41NDigis->Fill(b_ME41NDigis);
+
+      h_S3NDigis->Fill(S3NDigis);
+      h_S4NDigis->Fill(S4NDigis);
+      Nrechit->Fill(nRPC);
+      h_S3Nrechits->Fill(S3Nrechits);
+      h_S4Nrechits->Fill(S4Nrechits);
+
 /*
         cout << "DetId: " << csc_id << endl;
-
         b_CSCendcap = (*csc).first.endcap()-1;
         b_CSCstation = (*csc).first.station()-1;
         b_CSCsector  = (*csc).first.triggerSector()-1;
@@ -293,17 +424,14 @@ CSCExtrapoltoRPC::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
                                " strip: "      << b_CSCstrip <<
                                " wire: "       << b_CSCkeyWire << endl;
         cout << "BX = " << b_cscBX << endl;
-
         cout << "and I'm with CSCDetId: endcap: "    << csc_id.endcap() <<
                                      " station: "    << csc_id.station() <<
                                      " ring: "     << csc_id.ring() <<
                                      " chamber: "  << csc_id.chamber() <<
                                      " layer: "      << csc_id.layer() << endl;
-
         //to check forward and backward endcap
         if ( b_CSCendcap == 0 ) b_fNDigis++; 
         if ( b_CSCendcap == 1 ) b_bNDigis++; 
-
         if ( b_CSCstation == 2 ){
           if ( b_cscId == 1 || b_cscId == 2 || b_cscId == 3 ) b_ME31NDigis++;
         }
@@ -314,89 +442,16 @@ CSCExtrapoltoRPC::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
       cout << "\nnumber of digis: " << b_numberofDigis << endl;   
       cout << "ME31 NDigis: " << b_ME31NDigis << " ME41 NDigis: " << b_ME41NDigis << endl;   
-
+*/
       //fill histo
-      Ndigis->Fill(b_numberofDigis);
-      if (b_fNDigis != 0) fNdigis->Fill(b_fNDigis);
-      if (b_bNDigis != 0) bNdigis->Fill(b_bNDigis);
-      if (b_ME31NDigis != 0) ME31NDigis->Fill(b_ME31NDigis);
-      if (b_ME41NDigis != 0) ME41NDigis->Fill(b_ME41NDigis);
-*/
+//      Ndigis->Fill(b_numberofDigis);
+//      if (b_fNDigis != 0) fNdigis->Fill(b_fNDigis);
+//      if (b_bNDigis != 0) bNdigis->Fill(b_bNDigis);
+//      if (b_ME31NDigis != 0) ME31NDigis->Fill(b_ME31NDigis);
+//      if (b_ME41NDigis != 0) ME41NDigis->Fill(b_ME41NDigis);
+
   }
 
-  int numberofRecHits = 0;
-  int nrechitRE31 = 0;
-  int nrechitRE41 = 0;
-  int nRPC = 0;
-  for (RPCRecHitCollection::const_iterator rpcIt = rpcRecHits->begin(); rpcIt != rpcRecHits->end(); rpcIt++) {
-    RPCDetId rpcId = (*rpcIt).rpcId();
-    typedef std::pair<RPCRecHitCollection::const_iterator, RPCRecHitCollection::const_iterator> rangeRecHits;
-    rangeRecHits recHitCollection = rpcRecHits->get(rpcId);
-    RPCRecHitCollection::const_iterator recHit;
-
-    numberofRecHits = nrechitRE31 = nrechitRE41 = 0;
-    
-    for (recHit = recHitCollection.first; recHit != recHitCollection.second; recHit++){
-//    bool isNewChamber = true;
-      nRPC++;
-      numberofRecHits++;
-      RPCDetId rpcid = (*rpcIt).rpcId();
-//      GlobalPoint gp_rpc(getRPCGlobalPosition(rpcid, *rpcIt));
-      GlobalPoint gp_rpc(getRPCGlobalPosition(rpcid, *recHit));
-
-      if (rpcid.region() == -1 || rpcid.region() == 1){
-        if (rpcid.station() == 3 && rpcid.ring() == 1){
-          nrechitRE31++;
-          cout << "RPCGlobalposition in ME3/1" << gp_rpc << "BX/clSize: " << (*rpcIt).BunchX()  << "/" << (*rpcIt).clusterSize() << endl;
-        }
-        else if (rpcid.station() == 4 && rpcid.ring() == 1){
-          nrechitRE41++;
-          cout << "RPCGlobalposition in ME4/1" << gp_rpc << "BX/clSize: " << (*rpcIt).BunchX() << "/" << (*rpcIt).clusterSize() << endl;
-        }
-      }
-
-    }
-    
-    Nrechit->Fill(numberofRecHits);
-    RE31Nrechit->Fill(nrechitRE31);
-    RE41Nrechit->Fill(nrechitRE41);
-/*
-    int kRoll  = rpcid.roll();
-    int kSubsector  = rpcid.subsector();
-    int kRegion  = rpcid.region();
-    int kStation = rpcid.station();
-    int kRing = rpcid.ring();
-    int kSector = rpcid.sector();
-    int kLayer = rpcid.layer();
-    int bx = (*rpcIt).BunchX();
-    int clSize = (*rpcIt).clusterSize();
-
-    cout << "I'm here in RPC Region: " << kRegion <<
-                          " Station: " << kStation <<
-                          " Ring: "    << kRing <<
-                          " Sector: "  << kSector <<
-                        " Subsector: " << kSubsector <<
-                             " Roll: " << kRoll <<
-                            " Layer: " << kLayer <<
-                       "\tbx,clSize: " << bx << ", " << clSize << endl;    
-    cout << "number of RPCRecHits: " << nRPC << endl;
-
-    int nrechitRE31 = 0;
-    int nrechitRE41 = 0;
-    if (kRegion != 0){
-      if (kStation == 3 && kRing == 1) nrechitRE31++;    
-      else if(kStation == 4 && kRing == 1) nrechitRE41++;
-
-    }
-
-    cout << "RE31: " << nrechitRE31 << " RE41: " << nrechitRE41 << endl;
-    if (nrechitRE31 != 0) RE31Nrechit->Fill(nrechitRE31);
-    if (nrechitRE41 != 0) RE41Nrechit->Fill(nrechitRE41);
-*/
-
-
-   
-  }
 
   tree->Fill();
   EventInfo->Fill(1.5);
