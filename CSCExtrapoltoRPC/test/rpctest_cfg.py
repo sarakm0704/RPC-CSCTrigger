77b3744850593a3891a6777257a373d4f82aa851
Jieun <crown_1225@naver.com>
2019-04-19 09:01:12 +0200

    config update

--- CSCExtrapoltoRPC/test/rpctest_cfg.py
+++ CSCExtrapoltoRPC/test/rpctest_cfg.py
@@ -3,6 +3,15 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("rpcNtupler")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "93X_upgrade2023_realistic_v5"
+#process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
 
 from RecoLocalMuon.RPCRecHit.rpcRecHits_cfi import rpcRecHits
 process.rpcRecHits = rpcRecHits
@@ -15,12 +24,11 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
        'file:/u/user/jichoi/WORK/RPC+CSCTrigger/CMSSW_9_3_13/src/RPC-CSCTrigger/CSCExtrapoltoRPC/test/rpcoutput-testone.root'
+#        'file:/u/user/jichoi/WORK/RPC+CSCTrigger/CMSSW_9_3_13/src/data/loggs/RPCoutput_withRecHitCSC_101.root',
     )
 )
 
 process.rpcntupler = cms.EDAnalyzer("CSCExtrapoltoRPC",
-   cscSegments = cms.InputTag('cscSegments'),
-   tracks = cms.untracked.InputTag('tracks'),
 #   rpcRecHits = cms.InputTag('rpcRecHits'),
 #   simMuonRPCDigis = cms.InputTag('simMuonRPCDigis'),
    simMuonRPCDigis = cms.InputTag('rpcRecHits'),
@@ -28,7 +36,7 @@ process.rpcntupler = cms.EDAnalyzer("CSCExtrapoltoRPC",
 )
 
 process.TFileService = cms.Service("TFileService",
-     fileName = cms.string('rpcNtuple.root')
+     fileName = cms.string('rpcNtuple1.root')
  )
 
 process.p = cms.Path(process.rpcntupler)
